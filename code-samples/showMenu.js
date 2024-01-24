1303bbb2ee06dc2ef5eb1a219b668e1da17829e7
Revital Kaisar <revital.kaisar@gmail.com>
2024-01-24 10:42:49 -0500

    Delete code-samples directory

--- code-samples/showMenu.js
+++
@@ -1,8 +0,0 @@
-function showMenu(){
-        var  menu = document.getElementById("topmenu");
-        if  (menu.style.display==="none"){
-            menu.style.display="block";
-        } else {
-        menu.style.display="none";
-            }
-        }
